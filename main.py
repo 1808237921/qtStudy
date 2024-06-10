c9004033e333803a7fa3669b8c0df6bd425e2404
zikun <zikun@stu.xjtu.edu.cn>
2024-06-10 21:40:28 +0800

    add inference

--- main.py
+++ main.py
@@ -5,9 +5,26 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from Ui_MainWindow import Ui_MainWindow
 from PIL import Image
-import onnx
-import onnxruntime as ort
+import torch
+from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor
+from nnunetv2.imageio.simpleitk_reader_writer import SimpleITKIO
 
+predictor = nnUNetPredictor(
+    tile_step_size=0.5,
+    use_gaussian=True,
+    use_mirroring=True,
+    perform_everything_on_device=False,
+    device=torch.device('cuda'),
+    verbose=False,
+    verbose_preprocessing=False,
+    allow_tqdm=True
+)
+
+predictor.initialize_from_trained_model_folder(
+    "3d_lowres_model",
+    use_folds=(0,),
+    checkpoint_name='checkpoint_final.pth',
+)
  
 class Window(QMainWindow, Ui_MainWindow): 
     def __init__(self, parent=None):
@@ -21,6 +38,7 @@ class Window(QMainWindow, Ui_MainWindow):
         self.horizontalSlider.sliderMoved.connect(self.onSliderMoved)
         self.hasImageOn = False
         self.hasInfered = False
+        self.imagePath = None
 
     def onAction2(self):
         QMessageBox.information(self, "关于", "这是一个用Qt开发的demo", QMessageBox.Ok)
@@ -29,6 +47,7 @@ class Window(QMainWindow, Ui_MainWindow):
         filepath, _ = QFileDialog.getOpenFileName(self, "打开", "c:", "nii.gz文件(*.nii.gz);; nii文件 (*.nii)")
         if filepath == "": return
         self.hasImageOn = True
+        self.imagePath = filepath
         self.data = sitk.ReadImage(filepath)
         self.data = sitk.GetArrayFromImage(self.data)
         self.horizontalSlider.setMaximum(self.data.shape[0] - 1)
@@ -42,25 +61,20 @@ class Window(QMainWindow, Ui_MainWindow):
         if not self.hasImageOn:
             QMessageBox.information(self, "提示", "请选择一个nii文件再进行推理!", QMessageBox.Ok)
             return
-        #### 进行推理
-
-        ####
-
-        #### 暂时填充原图
-        image = Image.fromarray(np.uint8(self.data[self.horizontalSlider.value()])).toqpixmap()
+        image, props = SimpleITKIO().read_images([self.imagePath])
+        self.data_infered = predictor.predict_single_npy_array(image, props, None, None, False)
+        self.data_infered[self.data_infered == 1] = 255
+        image = Image.fromarray(np.uint8(self.data_infered[self.horizontalSlider.value()])).toqpixmap()
         self.label_2.setPixmap(image)
-        ####
         self.hasInfered = True
-        pass
 
     def onSliderMoved(self, val):
         if not self.hasImageOn: return
         image = Image.fromarray(np.uint8(self.data[val])).toqpixmap()
         self.label.setPixmap(image)
         if self.hasInfered:
-            #### 暂时填充原图
+            image = Image.fromarray(np.uint8(self.data_infered[val])).toqpixmap()
             self.label_2.setPixmap(image)
-            ####
         
         
 if __name__ == "__main__":
