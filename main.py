d894e54d1dc3b13580bc794f7246a4af580f36ed
zikun <zikun@stu.xjtu.edu.cn>
2024-06-11 21:02:08 +0800

    implement save functions

--- main.py
+++ main.py
@@ -46,34 +46,44 @@ class Window(QMainWindow, Ui_MainWindow):
         QMessageBox.information(self, "关于", "这是一个用Qt开发的demo", QMessageBox.Ok)
 
     def onAction3Triggered(self):
-        filepath, _ = QFileDialog.getOpenFileName(self, "打开", "c:", "nii.gz文件(*.nii.gz);; nii文件 (*.nii)")
+        filepath, _ = QFileDialog.getOpenFileName(self, "打开", ".", "nii.gz文件(*.nii.gz);; nii文件 (*.nii)")
         if filepath == "": return
         self.hasImageOn = True
         self.imagePath = filepath
         self.data = sitk.ReadImage(filepath)
-        self.data = (1 - sitk.GetArrayFromImage(self.data)) * 255
+        self.data = np.uint8((1 - sitk.GetArrayFromImage(self.data)) * 255)
         self.horizontalSlider.setMaximum(self.data.shape[0] - 1)
         self.horizontalSlider.setValue(0)
-        image = Image.fromarray(np.uint8(self.data[0])).filter(ImageFilter.MedianFilter)
+        image = Image.fromarray(self.data[0]).filter(ImageFilter.MedianFilter)
         self.label.setPixmap(image.toqpixmap())
         self.label_2.setText("等待推理")
         self.hasInfered = False
 
     def onAction4Triggered(self):
         if not self.hasImageOn:
-            QMessageBox.information(self, "提示", "请选择一个nii文件再进行推理!", QMessageBox.Ok)
+            QMessageBox.information(self, "提示", "请选择一个nii文件再进行推理！", QMessageBox.Ok)
             return
         image, props = SimpleITKIO().read_images([self.imagePath])
-        self.data_infered = predictor.predict_single_npy_array(image, props, None, None, False) * 255
-        image = Image.fromarray(np.uint8(self.data_infered[self.horizontalSlider.value()]))
+        self.data_infered = np.uint8(predictor.predict_single_npy_array(image, props, None, None, False) * 255)
+        image = Image.fromarray(self.data_infered[self.horizontalSlider.value()])
         self.label_2.setPixmap(image.toqpixmap())
         self.hasInfered = True
 
     def onAction5Triggered(self):
-        pass
-
+        if not self.hasImageOn: 
+            QMessageBox.information(self, "提示", "请打开一个nii文件再进行保存原图操作！", QMessageBox.Ok)
+            return
+        filepath, _ = QFileDialog.getSaveFileName(self, "保存", ".", "jpg文件(*.jpg);; png文件 (*.png)")
+        image = Image.fromarray(self.data[self.horizontalSlider.value()])
+        image.save(filepath)
+        
     def onAction6Triggered(self):
-        pass
+        if not self.hasInfered:
+            QMessageBox.information(self, "提示", "请进行推理后再进行保存推理后的图片操作！", QMessageBox.Ok)
+            return
+        filepath, _ = QFileDialog.getSaveFileName(self, "保存", ".", "jpg文件(*.jpg);; png文件 (*.png)")
+        image = Image.fromarray(self.data_infered[self.horizontalSlider.value()])
+        image.save(filepath)
 
     def onSliderMoved(self, val):
         if not self.hasImageOn: return
